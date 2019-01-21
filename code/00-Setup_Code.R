91f00704b063feb8f466ac747fd17ad8306c8cda
Elliot Palmer <epalmer@victoria.com>
2019-01-20 23:46:38 -0500

    update mapping post

---
+++ code/00-Setup_Code.R
@@ -0,0 +1,6 @@
+library(blogdown)
+#https://github.com/themefisher/meghna-hugo.git
+#https://github.com/jpescador/hugo-future-imperfect.git
+install_theme("jpescador/hugo-future-imperfect", theme_example = TRUE, update_config = TRUE)
+
+blogdown::serve_site()
